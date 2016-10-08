235862dfee08fbd7f82d014b369eab62c68f0793
Maxim Solodovnik <solomax666@gmail.com>
2016-10-08 12:22:40 +0700

    Initial commit

---
+++ multimodule-with-war-core/src/main/java/org/apache/multimodule_with_war/core/App.java
@@ -0,0 +1,13 @@
+package org.apache.multimodule_with_war.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ multimodule-with-war-util/src/main/java/org/apache/multimodule_with_war/util/App.java
@@ -0,0 +1,13 @@
+package org.apache.multimodule_with_war.util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
