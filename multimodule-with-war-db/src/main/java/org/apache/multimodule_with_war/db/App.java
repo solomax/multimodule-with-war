07c8ac6da57a3900744e88bb52d3675718213bb6
Maxim Solodovnik <solomax666@gmail.com>
2017-09-05 15:48:19 +0700

    Internal dependency is added, license plugin is moved to profile

--- multimodule-with-war-db/src/main/java/org/apache/multimodule_with_war/db/App.java
+++ multimodule-with-war-db/src/main/java/org/apache/multimodule_with_war/db/App.java
@@ -1,13 +1,14 @@
 package org.apache.multimodule_with_war.db;
 
+import org.apache.multimodule_with_war.util.UtilTest;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		new UtilTest();
+		System.out.println("Hello World!");
+	}
 }
---
+++ multimodule-with-war-util/src/main/java/org/apache/multimodule_with_war/util/UtilTest.java
@@ -0,0 +1,7 @@
+package org.apache.multimodule_with_war.util;
+
+public class UtilTest {
+	public UtilTest() {
+		System.out.println("No-op UtilTest constructor");
+	}
+}
